14e60dc39033bb2c52db4be18d33a04fe611837f
Robin GROSS <w.robingross@gmail.com>
2018-05-16 23:20:03 +0200

    design funding on landing page

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,11 +1,18 @@
 import { Component, AfterContentInit } from '@angular/core';
 
 @Component({
-    selector: 'landing',
+    selector: 'app-landing',
     templateUrl: './landing.component.html'
 })
 
 export class LandingComponent {
+
+    offers: any  = [
+      {active: false, type: 'Starter', pricing: 'free', storage: '30Gb' },
+      {active: true, type: 'Developer', pricing: '15$ /Mo', storage: '500Gb' },
+      {active: false, type: 'Business', pricing: '30$ /Mo', storage: '2To' },
+      {active: false, type: 'Compagnies', pricing: '60$ /Mo', storage: '10To' }
+    ];
 
     constructor() {}
 }
