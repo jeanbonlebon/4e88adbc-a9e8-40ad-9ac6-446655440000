043eb22f8ffa8be4ec7bccc8e91beb1015ae4989
Robin GROSS <w.robingross@gmail.com>
2018-05-21 11:56:58 +0200

    shipit conf to remote

--- shipitfile.js
+++ shipitfile.js
@@ -1,15 +1,39 @@
 module.exports = shipit => {
     
     shipit.initConfig({
+        default: {  
+            key: '/home/dev/.ssh/id_rsa',
+            keepReleases: 2,
+        },
         staging: {
-            servers: 'dev@192.168.222.146',
+            servers: 'dev@192.168.222.147',
         },
     })
 
     shipit.task('copyConfig', async () => {
         await shipit.copyToRemote(
-            'package.json',
-            '/home/dev/package.json',
+            '/home/dev/4e88adbc-a9e8-40ad-9ac6-446655440000/build',
+            '/var/www/supfile',
         )
+    })
+
+    shipit.task('deploy_front', function() {
+        shipit.local('git pull origin master')
+        .then(function(res) {
+            console.log('Angular App is Updated with Repository')
+            shipit.local('npm run build')      
+            .then(function(res) {
+                console.log('Production App is Builded')
+                shipit.remote('rm -fr /var/www/supfile')
+                .then(function(res) {
+                    shipit.task('copyConfig', async () => {
+                        await shipit.copyToRemote(
+                            '/home/dev/4e88adbc-a9e8-40ad-9ac6-446655440000/build',
+                            '/var/www/supfile',
+                        )
+                    })
+                })
+            })
+        })
     })
 }
